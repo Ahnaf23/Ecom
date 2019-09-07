5bb29fcd61cd30cadef183d1f66ff7a3d5b27777
ahnaf <ahnafsakibalam@yahoo.com>
2019-09-07 15:54:39 +1000

    Latest

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,5 +1,7 @@
-#i have created this file
+from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Hiiiiiii")
+
+    return render(request, 'index.html')
