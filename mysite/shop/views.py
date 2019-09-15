fc950265bc5105ebe9deb4c7ec0a7c2919df9670
ahnaf <ahnafsakibalam@yahoo.com>
2019-09-15 18:13:11 +1000

    Latest

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -6,6 +6,8 @@ import json
 
 # Create your views here.
 #
+
+
 def index(request):
 
 
@@ -22,8 +24,36 @@ def index(request):
     params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
 
+
+def searchMatch(query, item):
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+
+def search(request):
+    query = request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod)!=0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+
+    params = {'allProds': allProds, "msg": ""}
+    if len(allProds) == 0:
+        params = {'msg': "Cannot Find Anything, Sorry!"}
+    return render(request, 'shop/search.html', params)
+
+
 def about(request):
     return render(request, 'shop/about.html')
+
 
 def contact(request):
     if request.method=="POST":
@@ -36,6 +66,7 @@ def contact(request):
         contact.save()
     return render(request, 'shop/contact.html')
 
+
 def tracker(request):
     if request.method == "POST":
         orderId = request.POST.get('orderId', '')
@@ -47,7 +78,7 @@ def tracker(request):
                 updates = []
                 for item in update:
                     updates.append({'text': item.update_desc, 'time': item.timestamp})
-                    response = json.dumps([updates, order[0].items_json], default=str)
+                    response = json.dumps({"status": "success", "updates": updates, "itemsJson": order[0].items_json}, default=str)
                 return HttpResponse(response)
             else:
                 return HttpResponse('{}')
@@ -56,8 +87,8 @@ def tracker(request):
 
     return render(request, 'shop/tracker.html')
 
-def search(request):
-    return render(request, 'shop/search.html')
+
+
 
 def productview(request, myid):
     product = Product.objects.filter(id=myid)
