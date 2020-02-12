b2c37ab8491f91d8edb0fa9274d20f4957c8aaff
ahnaf <ahnafsakibalam@yahoo.com>
2020-02-12 19:56:37 +1100

    Changed products

--- mysite/ausPostApi/api.py
+++ mysite/ausPostApi/api.py
@@ -1,8 +1,9 @@
 
 import json
 from bottle import Bottle, template, static_file, request, response, redirect, HTTPError
-import requests
 import urllib.parse
+import requests
+
 
 app = Bottle()
 
@@ -18,19 +19,16 @@ def calc():
     url = "https://digitalapi.auspost.com.au/postage/parcel/domestic/calculate.json?"
     params = {
         "from_postcode": request.forms.get("from_postcode"),
-          "to_postcode": request.forms.get("to_postcode"),
-          "length":"22",
-          "width":"16",
-          "height":"7.7",
-          "weight":"1.5",
-          "service_code":"AUS_PARCEL_REGULAR"}
+        "to_postcode": request.forms.get("to_postcode"),
+        "length":"50",
+        "width":"40",
+        "height":"10",
+        "weight":"2",
+        "service_code":"AUS_PARCEL_REGULAR"}
     generatedTail = urllib.parse.urlencode(params)
     jsonData = requests.get(url+generatedTail, headers = {"AUTH-Key":apiKey})
     print(jsonData.text)
-    message = {
-        'cost' : "python",
-    }
-    return template("index", message = "hello")
+    template("index.tpl", "hello")
 
 
 if __name__ == '__main__':
